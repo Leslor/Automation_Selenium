575f192fe1f4d88cb74945601cdbaac9fe460ccb
Leslor_P <leslie.paz.am@gmail.com>
2021-09-24 14:54:24 -0500

    Se agregan comentarios a cada test

--- find_element.py
+++ find_element.py
@@ -12,13 +12,14 @@ class HomePageTests(unittest.TestCase):
         driver=self.driver
         driver.get('https://qaclubcare.dinersclubperu.pe/')
         driver.maximize_window() 
-                
+    
+    #probar buscador
     def test_search_text_field_by_name(self):
         search_field=self.driver.find_elements_by_name("buscador")
-
+    #probar boton VerPlanes
     def test_verplanes_button_enabled(self):
         button=self.driver.find_element_by_class_name("btnPlanes")
-
+    #Validar n° de seguros
     def test_count_of_insurance_nav(self):
         insurance_list=self.driver.find_element_by_class_name("boxMenuPC")
         insurance=insurance_list.find_elements_by_tag_name('li')
